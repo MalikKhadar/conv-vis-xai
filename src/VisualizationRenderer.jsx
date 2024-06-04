966826cfe0a308642f8aac5bd50223581c449031
MalikKhadar <khada005@umn.edu>
2024-06-04 13:23:05 -0500

    Updated notes for visualizations

--- src/VisualizationRenderer.jsx
+++ src/VisualizationRenderer.jsx
@@ -13,11 +13,11 @@ const VisualizationRenderer = ({ parentState, defaultMessage }) => {
   const renderSwitch = (index) => {
     switch (index) {
       case 0:
-        return <img src="https://raw.githubusercontent.com/MalikKhadar/conv-vis-xai/main/src/assets/datapoint/income_waterfall.png" style={{ maxHeight: "60vh" }} />
+        return <div><img src="https://raw.githubusercontent.com/MalikKhadar/conv-vis-xai/main/src/assets/datapoint/income_waterfall.png" style={{ maxHeight: "60vh" }} /><p>In this plot, negative f(x) values indicate "less than $50k" classifications, whereas positive f(x) values indicate the opposite</p></div>
       case 1:
-        return <div style={{ width: "100%", maxHeight: "60vh" }}><ComparisonTable tableData={simData} />(The model made <b>the same prediction</b> on each of these data points. The first row is the main data point)</div>
+        return <div style={{ width: "100%", maxHeight: "60vh" }}><ComparisonTable tableData={simData} />(The model made <b>the same prediction</b> on each of these data points. The first row is the main data point. Cells are gold when they share the same value as the main data point)</div>
       case 2:
-        return <div style={{ width: "100%", maxHeight: "60vh" }}><ComparisonTable tableData={cfData} />(The model made <b>the opposite prediction</b> for each of these data points when compared to the main data point in the first row)</div>;
+        return <div style={{ width: "100%", maxHeight: "60vh" }}><ComparisonTable tableData={cfData} />(The model made <b>the opposite prediction</b> for each of these data points when compared to the main data point in the first row. Cells are gold when they share the same value as the main data point)</div>;
       default:
         return <p>{defaultMessage}</p>;
     }
