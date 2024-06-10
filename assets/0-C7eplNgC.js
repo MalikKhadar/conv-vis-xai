c64cce16b0c143ec3fc39f9156095ecdd038a469
MalikKhadar <khada005@umn.edu>
2024-06-10 13:35:42 -0500

    Updates

---
+++ assets/0-C7eplNgC.js
@@ -0,0 +1,1 @@
+const s="/conv-vis-xai/assets/0-D9j49_RC.png";export{s as default};
---
+++ assets/questions-DIb44Edj.js
@@ -0,0 +1,4 @@
+const e=[{question:"Do you agree with the model's prediction? Why or why not?",options:["Yes","No"],explain:!0},{question:`Would the model make the same prediction given these modifications to the features? 
+Capital loss = 0`,options:["Yes","No"],explain:!1},{question:`Would the model make the same prediction given these modifications to the features? 
+Occupation = Prof-specialty
+Education-Num = 14`,options:["Yes","No"],explain:!1},{question:"What is the most important feature for this prediction?",options:["Capital Loss","Workclass","Capital Gain","Relationship"],explain:!1}];export{e as default};
