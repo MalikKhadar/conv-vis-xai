dc79af9b820f9fe4bb340d045fd248ca52b8eaee
MalikKhadar <khada005@umn.edu>
2024-07-19 16:23:19 -0500

    Updates

--- assets/questions-CmKOFQXq.js
+++ assets/questions-CmKOFQXq.js
@@ -1,4 +1,4 @@
 const e=[{question:"Do you think the model's prediction is accurate? Why or why not?",options:["Yes","No"],explain:!0},{question:`Would the model make the same prediction given these modifications to the features? 
 Country = United-States
-Education-Num = 10`,options:["Yes","No"],explain:!1},{question:`Would the model make the same prediction given these modifications to the features? 
+Education = Some-college`,options:["Yes","No"],explain:!1},{question:`Would the model make the same prediction given these modifications to the features? 
 Country = England`,options:["Yes","No"],explain:!1},{question:"What is the most important feature for this prediction?",options:["Capital Loss","Occupation","Capital Gain","Age"],explain:!1}];export{e as default};
